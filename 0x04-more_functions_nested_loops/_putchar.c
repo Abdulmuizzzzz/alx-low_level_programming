f9eb52422e8e4d4bb0a7996a4d52da22f9d68fa4
Abdulmuiz <dsulaiman12300@gmail.com>
2023-02-23 19:50:03 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * _putchar - writes the character in c to stdout
+ * @c: charcter to be used
+ * Return: On success 1.
+ * On error, -1 is returned.
+ */
+
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
